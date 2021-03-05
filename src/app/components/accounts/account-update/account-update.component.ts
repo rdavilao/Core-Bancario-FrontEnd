dc6f1c8b97e7aa13d2ff0c529955b4f87530fe73
Daniel <rdavila@espe.edu.ec>
2021-03-05 12:13:15 -0500

    Updated account-list and created components of credit-card.

--- src/app/components/accounts/account-update/account-update.component.ts
+++ src/app/components/accounts/account-update/account-update.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../../../services/account.service';
 
 @Component({
   selector: 'app-account-update',
   templateUrl: './account-update.component.html',
-  styleUrls: ['./account-update.component.css']
+  styleUrls: ['./account-update.component.css'],
+  providers: [AccountService]
 })
 export class AccountUpdateComponent implements OnInit {
 
-  constructor() { }
+  public number: string;
+
+  constructor(
+    private _accountService: AccountService
+  ) {
+    this.number = "";
+   }
 
   ngOnInit(): void {
+  }
+
+  confirm(){    
+    this._accountService.updateStateAccount(this.number).subscribe();
   }
 
 }
---
+++ src/app/components/creditCardPayment/credit-card/credit-card.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Transaction } from '../../../models/transaction';
+import { TransactionService } from '../../../services/transaction.service';
+
+@Component({
+  selector: 'app-credit-card',
+  templateUrl: './credit-card.component.html',
+  styleUrls: ['./credit-card.component.css'],
+  providers: [TransactionService]
+})
+export class CreditCardPaymentComponent implements OnInit {
+
+  public title: string;
+  public transaction: Transaction;
+  public status: string;
+  public showT: string;
+  public img: string;
+
+  constructor(
+    private _transactionService: TransactionService
+  ) {
+    this.title = "Pago de tarjetas de crédito.";
+    this.transaction = new Transaction('', '', '', new Date(), 'Pago Tarjeta', '', 0, 0);
+  }
+
+  ngOnInit(): void {
+  }
+
+  newTransaction(form){
+    
+    this.showT = '';
+    this.img = "";
+    this.status = '';
+    form.reset(); 
+  }
+
+  onSubmit(form) {
+    this.transaction.identification = this.transaction.account;
+    this.transaction.description = "Pago con monto de: " + this.transaction.mont + " de la tarjeta  " +
+      this.transaction.account + ", saldo pendiente: " + this.transaction.balanceAccount;
+    this._transactionService.saveTransactionTC(this.transaction).subscribe(
+      response => {
+        if (response == null) {
+          this.status = 'success';
+          this.showT = 'success';
+          this.img = "../../../../assets/logo-bb-4.jpeg";
+        } else {
+          this.status = 'failed';
+        }
+      },
+      error => {
+        this.showT = '';
+        this.img = "";
+        console.log(<any>error);
+      }
+    )
+  }
+
+}
